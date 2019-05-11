c96071a03c41bf48dcf6224fc406c966d6fa27b9
shui <164635470@qq.com>
2019-05-11 23:44:01 +0800

    setting

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -4,6 +4,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('sayhello')
-
+app.config.from_pyfile('settings.py')#加载配置
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 db = SQLAlchemy(app)
+
+from sayhello import views, errors, commands
